1ca20baf35355390d43c134e79c351aea9332cfa
Jeff Lapp <lappjeff20@gmail.com>
2019-05-28 16:57:14 -0400

    Further work on properties for deployment
    Added further lines for use in eventual postgres/heroku deployment

--- src/main/java/lambda/macrocalculator/repos/UserRepository.java
+++ src/main/java/lambda/macrocalculator/repos/UserRepository.java
@@ -4,11 +4,13 @@ import lambda.macrocalculator.model.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends CrudRepository<User, Long>
 {
 	User findByUsername(String name);
 
+	@Transactional
 	@Modifying
 	@Query(value = "DELETE FROM userroles WHERE userid = :userid", nativeQuery = true)
 	void deleteUserRole(long userid);
