e7d214ff90c7d35436e985adeb3aea2e421362a0
Jeff Lapp <lappjeff20@gmail.com>
2019-05-28 11:40:57 -0400

    Set up initial Java project

---
+++ src/main/java/lambda/macrocalculator/MacroCalculatorApplication.java
@@ -0,0 +1,15 @@
+package lambda.macrocalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroCalculatorApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MacroCalculatorApplication.class, args);
+	}
+
+}
