f8ea5a66b0666a13ac98933d8df1cd428a133e41
Jeff Lapp <lappjeff20@gmail.com>
2019-05-28 17:18:33 -0400

    Enabled auditing
    Added files and annotations to fully enable auditing

---
+++ src/main/java/lambda/macrocalculator/service/UserAuditing.java
@@ -0,0 +1,31 @@
+package lambda.macrocalculator.service;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserAuditing implements AuditorAware<String>
+{
+
+	@Override
+	public Optional<String> getCurrentAuditor()
+	{
+		String uname;
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication != null)
+		{
+			uname = authentication.getName();
+		}
+		else
+		{
+			uname = "SYSTEM";
+		}
+		return Optional.of(uname);
+	}
+
+}
