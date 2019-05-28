da725e8f9ce0cf36a3412f4f7e144c93f1894d58
Jeff Lapp <lappjeff20@gmail.com>
2019-05-28 17:17:54 -0400

    Update ResourceServerConfig.java
    All user paths other than account creation now protected

--- src/main/java/lambda/macrocalculator/config/ResourceServerConfig.java
+++ src/main/java/lambda/macrocalculator/config/ResourceServerConfig.java
@@ -36,7 +36,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter
                                    "/webjars/**",              // swagger
                                     "/users/create"
                         ).permitAll()
-//                .antMatchers("/**").authenticated()
+                .antMatchers("/users/**").authenticated()
             .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
 
         // http.requiresChannel().anyRequest().requiresSecure();
