87b24b2584d3e44b5af36e1ad7eb2fe0593c2351
Jeff Lapp <lappjeff20@gmail.com>
2019-05-30 16:59:29 -0400

    Update MacrosController.java
    Removed unncessary create macro endpoint

--- src/main/java/lambda/macrocalculator/controller/MacrosController.java
+++ src/main/java/lambda/macrocalculator/controller/MacrosController.java
@@ -3,7 +3,6 @@ package lambda.macrocalculator.controller;
 import io.swagger.annotations.ApiOperation;
 import lambda.macrocalculator.exception.ResourceNotFoundException;
 import lambda.macrocalculator.model.Macros;
-import lambda.macrocalculator.model.User;
 import lambda.macrocalculator.service.MacrosService;
 import lambda.macrocalculator.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.security.Principal;
 
 @RestController
@@ -23,14 +21,6 @@ public class MacrosController
 
 	@Autowired
 	private UserService userService;
-
-	@ApiOperation("Takes in inches and meal plan then returns macros calculation based off that")
-	@PostMapping(value = "/create", consumes = "application/json", produces = "application/json")
-	public ResponseEntity<?> createMacros(Principal principal, @Valid @RequestBody Macros newMacros)
-	{
-		macrosService.save(newMacros, principal);
-		return new ResponseEntity<>(findMacros(principal).getBody(), HttpStatus.CREATED);
-	}
 
 	@ApiOperation("Returns macros based off of current user")
 	@GetMapping(value = "/getmacros", produces = "application/json")
