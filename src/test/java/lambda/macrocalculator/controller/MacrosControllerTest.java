cf90f95962506467478385bba8cf6d2868d160f1
Jeff Lapp <lappjeff20@gmail.com>
2019-05-31 10:09:39 -0400

    Generated test boiler plate

---
+++ src/test/java/lambda/macrocalculator/controller/MacrosControllerTest.java
@@ -0,0 +1,31 @@
+package lambda.macrocalculator.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MacrosControllerTest
+{
+
+	@BeforeEach
+	void setUp()
+	{
+	}
+
+	@AfterEach
+	void tearDown()
+	{
+	}
+
+	@Test
+	void findMacros()
+	{
+	}
+
+	@Test
+	void updateMeals()
+	{
+	}
+}
